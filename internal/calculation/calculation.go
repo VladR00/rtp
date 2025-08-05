c0427ccbc6125ff04b6b8496daa045da8c5ede06
VladR00 <nazvaniepoch@gmail.com>
2025-08-05 09:53:20 +0300

    finished

--- internal/calculation/calculation.go
+++ internal/calculation/calculation.go
@@ -2,7 +2,6 @@ package calculation
 
 import (
 	"fmt"
-	"log"
 	"math"
 	"math/rand"
 	"time"
@@ -17,60 +16,65 @@ func NewStorage(rtp float64) *Storage {
 }
 
 func random(min float64, max float64) float64 {
+	if max > 10000 {
+		max = 10000
+	}
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 	rng := min + (random.Float64() * (max - min))
 	return math.Round(rng*10) / 10
 }
 
-func generateMultiplier(sum float64, count int, rtp float64) float64 {
-	var newMultiplier float64
-	rtpNow := sum / float64(count)
-	rtpNow = math.Round(rtpNow*10) / 10
-	// log.Println("sum: ", sum)
-	// log.Println("rtpNow: ", rtpNow)
-	// log.Println("rtpNeed: ", rtp)
-	if rtpNow > rtp {
-		newMultiplier = 1
-	} else {
-		newMultiplier = random(1, 10000)
+func generateMultiplier(sum float64, count int, allcount int, rtp float64, check *bool, seq float64) float64 {
+	sumFinal := rtp * float64(count)
+	sumNeed := sumFinal - sum + (float64(allcount) - float64(count))
+	if sumNeed < 0 {
+		sumNeed *= -1
 	}
-	return newMultiplier
+	if count <= 100 && *check == true && allcount-1 != count {
+		val := random(1, 1000)
+		if val > seq {
+			*check = false
+			//fmt.Println("Check turn false, i: ", count)
+		}
+		return val
+	}
+	fmt.Println(sumNeed)
+	if allcount-1 == count && *check == true {
+		val := random(1, 10000)
+		//fmt.Println("last case, Value:", val)
+		return val
+	}
+	return random(1, sumNeed)
 }
 
 func (storage *Storage) Calculation(count int) float64 {
-	var sequence []float64
-	var multiplier []float64
-	var transformed []float64
-	var sum float64
-	var allsum float64
+	//fmt.Println("start calc")
+	var sequence, multiplier []float64
+	var sum, allsum float64
+	var firstcheck bool = true
 
-	start := time.Now()
-	log.Println("Start goroutines")
 	for i := 0; i < count; i++ {
 		val := random(1, 10000)
 		sequence = append(sequence, val)
 		allsum += val
 	}
-	log.Println("End goroutines, len seq: ", len(sequence))
-	log.Println(time.Since(start))
+
 	for i := 0; i < count; i++ {
-		multiplier = append(multiplier, generateMultiplier(sum, i, storage.RTP))
+		multiplier = append(multiplier, generateMultiplier(sum, i, count, storage.RTP, &firstcheck, sequence[i]))
 		if multiplier[i] > sequence[i] {
-			transformed = append(transformed, sequence[i])
 			sum += sequence[i]
 		} else {
-			transformed = append(transformed, 0)
 		}
 	}
-	// log.Println("sequence", sequence)
-	// log.Println("multiplier", multiplier)
-	// log.Println("transformed", transformed)
-	log.Println("SUM: ", sum)
-	allsum = math.Round(allsum*10) / 10
-	sum = math.Round(sum*10) / 10
-	fmt.Printf("AllSum: %.2f\n", allsum)
+
 	rtp := sum / float64(count)
 	rtp = math.Round(rtp*10) / 10
-	log.Println(rtp)
+	allsum = math.Round(allsum*10) / 10
+	sum = math.Round(sum*10) / 10
+	// fmt.Println(multiplier)
+	// fmt.Printf("AllSum: %.1f\n", allsum)
+	// fmt.Printf("SUM:    %.1f\n", sum)
+	// fmt.Printf("NeedSum: %f\n", storage.RTP*float64(count))
+	// fmt.Println("RTP:   ", rtp)
 	return rtp
 }
