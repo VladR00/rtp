74ad50dcb4bca880a7a09a9993b26907b4f4034f
VladR00 <nazvaniepoch@gmail.com>
2025-08-05 20:54:25 +0300

    almost work

--- internal/calculation/calculation.go
+++ internal/calculation/calculation.go
@@ -27,31 +27,21 @@ func random(min float64, max float64) float64 {
 
 func generateMultiplier(sum float64, count int, allcount int, rtp float64, check *bool, seq float64) float64 {
 	sumFinal := rtp * float64(allcount)
-	sumNeed := sumFinal - sum + float64(allcount) - float64(count)
+	iterationforend := float64(allcount) - float64(count)
+	sumNeed := sumFinal - sum + iterationforend
+	fmt.Println("Iteration for end: ", iterationforend)
+	fmt.Println("Sum need: ", sumNeed)
+	fmt.Println("Current sum: ", sum)
+
 	if sum <= sumNeed {
-		fmt.Println("sum: ", sum)
-		fmt.Println("sumneed: ", sumNeed)
-		return random(1, 150)
-	}
-	fmt.Println("SumNeed: ", sumNeed)
-	if sumNeed < 0 {
-		sumNeed *= -1
-	}
-	if count <= 100 && *check == true && allcount-1 != count {
-		val := random(1, 1000)
-		if val > seq {
-			*check = false
-			fmt.Println("Check turn false, i: ", count)
+		if seq <= sumNeed {
+			return random(seq-1, 10000)
+		} else {
+			return random(1, seq-1)
 		}
-		return val
+	} else {
+		return random(1, seq-1)
 	}
-	fmt.Println(sumNeed)
-	if allcount-1 == count && *check == true && allcount <= 1000 {
-		val := random(1, 10000)
-		fmt.Println("last case, Value:", val)
-		return val
-	}
-	return random(1, sumNeed)
 }
 
 func (storage *Storage) Calculation(count int) float64 {
@@ -62,7 +52,7 @@ func (storage *Storage) Calculation(count int) float64 {
 	var firstcheck bool = true
 	chAllSum := make(chan float64, count)
 	wg := &sync.WaitGroup{}
-	var mu sync.Mutex
+	// var mu sync.Mutex
 	fmt.Println("start for")
 
 	for i := 0; i < count; i++ {
@@ -85,18 +75,18 @@ func (storage *Storage) Calculation(count int) float64 {
 	fmt.Println("Sequense len: ", len(sequence))
 
 	for i := 0; i < count; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			multiplier[i] = generateMultiplier(sum, i, count, storage.RTP, &firstcheck, sequence[i])
-			if multiplier[i] > sequence[i] {
-				mu.Lock()
-				defer mu.Unlock()
-				sum += sequence[i]
-			}
-		}()
+		// wg.Add(1)
+		// go func() {
+		// defer wg.Done()
+		multiplier[i] = generateMultiplier(sum, i, count, storage.RTP, &firstcheck, sequence[i])
+		if multiplier[i] > sequence[i] {
+			// mu.Lock()
+			// defer mu.Unlock()
+			sum += sequence[i]
+		}
+		// }()
 	}
-	wg.Wait()
+	// wg.Wait()
 
 	fmt.Println("multiplier len: ", len(multiplier))
 
